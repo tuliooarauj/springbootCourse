19aea461e56f03c5f639c6d5804b370a328fbdb2
tuliooarauj <127243520+tuliooarauj@users.noreply.github.com>
2023-10-18 09:58:50 -0300

    create springboot files

---
+++ src/main/java/br/edu/ifba/vscodesb/minicurso/MinicursoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.vscodesb.minicurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifba/vscodesb/minicurso/MinicursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.vscodesb.minicurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
